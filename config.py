14f44cc01dba03a312db6311e9e84c314042558d
utkarshbabar <utkarshbabarlaptop2006@gmail.com>
2025-11-03 04:30:20 +0530

    Remove Config class and dotenv initialization
    Remove configuration class and dotenv loading.

--- config.py
+++ config.py
@@ -1,13 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
-    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
-    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
